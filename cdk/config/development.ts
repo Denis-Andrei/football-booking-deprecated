8c9474f56ac085297c0511debd22fa51443f5323
Denis Peptanariu <denispeptanariu@gmail.com>
2024-05-03 14:51:11 +0100

    added api gateway url as env vars on amplify app

---
+++ cdk/config/development.ts
@@ -0,0 +1,6 @@
+import {FootballBookingInfraStackProps} from "../lib/football-booking-infra-stack";
+
+export const config: FootballBookingInfraStackProps = {
+    API_GATEWAY_URL: "https://f8xj4v5uv4.execute-api.eu-west-2.amazonaws.com/prod",
+    Region: "eu-west-2"
+}
--- cdk/lib/football-booking-infra-stack.ts
+++ cdk/lib/football-booking-infra-stack.ts
@@ -2,14 +2,29 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as amplify from 'aws-cdk-lib/aws-amplify';
 
+
+export interface FootballBookingInfraStackProps extends cdk.StackProps {
+    API_GATEWAY_URL: string;
+    Region: string;
+}
 export class FootballBookingInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: FootballBookingInfraStackProps) {
     super(scope, id, props);
 
     const footballBookingAmplifyApp = new amplify.CfnApp(this, 'FootballBookingAmplifyApp', {
         name: 'football-booking',
         repository: '',
         accessToken: cdk.SecretValue.secretsManager('github-access-token').unsafeUnwrap(),
+        environmentVariables: [
+            {
+                name: "API_GATEWAY_URL",
+                value: props.API_GATEWAY_URL
+            },
+            {
+                name: "REGION",
+                value: props.Region
+            }
+        ]
     });
 
      new amplify.CfnBranch(this, 'FootballBookingAmplifyBranch', {
