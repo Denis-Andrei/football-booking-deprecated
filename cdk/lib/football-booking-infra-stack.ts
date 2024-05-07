3ec67bf3e1df2bd4fd095591faf3029bab501bdd
Denis Peptanariu <denispeptanariu@gmail.com>
2024-05-07 15:36:50 +0100

    added cognito + auth

--- cdk/lib/football-booking-infra-stack.ts
+++ cdk/lib/football-booking-infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as amplify from 'aws-cdk-lib/aws-amplify';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 
 export interface FootballBookingInfraStackProps extends cdk.StackProps {
@@ -11,11 +12,43 @@ export class FootballBookingInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: FootballBookingInfraStackProps) {
     super(scope, id, props);
 
+    const userPool = new cognito.UserPool(this, 'FootballBookingUserPool', {
+        selfSignUpEnabled: true,
+        autoVerify: { email: true },
+        signInAliases: { email: true },
+    })
+
+   const userPoolClient =  new cognito.UserPoolClient(this, 'FootballBookingUserPoolClient', {
+        userPool: userPool,
+        generateSecret: false,
+    })
+
+  const identityPool = new cognito.CfnIdentityPool(this, 'FootballBookingIdentityPool', {
+        allowUnauthenticatedIdentities: false,
+        cognitoIdentityProviders: [{
+            clientId: userPoolClient.userPoolClientId,
+            providerName: userPool.userPoolProviderName
+        }]
+  })
+
+
     const footballBookingAmplifyApp = new amplify.CfnApp(this, 'FootballBookingAmplifyApp', {
         name: 'football-booking',
         repository: 'https://github.com/Denis-Andrei/football-booking',
         accessToken: cdk.SecretValue.secretsManager('github-access-token').unsafeUnwrap(),
         environmentVariables: [
+            {
+                name: "IDENTITY_POOL_ID",
+                value: identityPool.ref
+            },
+            {
+                name: "USER_POOL_ID",
+                value: userPool.userPoolId
+            },
+            {
+                name: "USER_POOL_CLIENT_ID",
+                value: userPoolClient.userPoolClientId
+            },
             {
                 name: "API_GATEWAY_URL",
                 value: props.API_GATEWAY_URL
--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+import {Authenticator} from "@aws-amplify/ui-react";
 import './App.css';
 
+
+import { awsConfig } from './aws-config';
+
+Amplify.configure(awsConfig);
 function App() {
-  const [showResult, setShowResult] = useState(false);
-  const [apiMessage, setApiMessage] = useState("");
-
-  const getApiMessage = async () => {
-
-    console.log(process.env.REACT_APP_API_GATEWAY_URL);
-    const response = await fetch(`${process.env.REACT_APP_API_GATEWAY_URL}/hello`, {
-      mode: 'no-cors'
-    });
-
-    const responseData = await response.text();
-    console.log(responseData)
-
-    setShowResult(true);
-    setApiMessage(responseData);
-  };
+  // const [showResult, setShowResult] = useState(false);
+  // const [apiMessage, setApiMessage] = useState("");
+  //
+  // const getApiMessage = async () => {
+  //
+  //   console.log(process.env.REACT_APP_API_GATEWAY_URL);
+  //   const response = await fetch(`${process.env.REACT_APP_API_GATEWAY_URL}/hello`, {
+  //     mode: 'no-cors'
+  //   });
+  //
+  //   const responseData = await response.text();
+  //   console.log(responseData)
+  //
+  //   setShowResult(true);
+  //   setApiMessage(responseData);
+  // };
 
 
   return (
-      <div className="App">
-        <header className="App-header">
-          <h1>CALL AN API</h1>
-          <button onClick={getApiMessage}>Call Lambda</button>
-          <div>
-            {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
-          </div>
-        </header>
-      </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+            <main>
+              <h1>Hello {user.username}</h1>
+              <button onClick={signOut}>Sign out</button>
+            </main>
+        )}
+      </Authenticator>
   );
 }
 
