7806d9bc2fe581897a1d038bc6102b03c6f20dc1
Denis Peptanariu <denispeptanariu@gmail.com>
2024-05-07 14:06:45 +0100

    changed params

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,10 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { FootballBookingInfraStack } from '../lib/football-booking-infra-stack';
-import {config} from "../config/development";
 
 const app = new cdk.App();
 new FootballBookingInfraStack(app, 'FootballBookingInfraStack', {
-    // at the moment only for the dev
-    ...config
+    API_GATEWAY_URL: "https://f8xj4v5uv4.execute-api.eu-west-2.amazonaws.com/prod",
+    Region: "eu-west-2"
 });
