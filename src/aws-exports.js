9f1eb83291718fef1253f844439b3cec771e7920
Denis Peptanariu <denispeptanariu@gmail.com>
2024-05-08 10:52:31 +0100

    removed aws-exports from gitignore

---
+++ src/aws-exports.js
@@ -0,0 +1,15 @@
+
+ const awsExports = {
+   Auth: {
+      Cognito: {
+         identityPoolId: `${process.env.REACT_APP_IDENTITY_POOL_ID}`,
+         region: `${process.env.REACT_APP_REGION}`,
+         identityPoolRegion: `${process.env.REACT_APP_REGION}`,
+         userPoolId: `${process.env.REACT_APP_USER_POOL_ID}`,
+         userPoolWebClient: `${process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID}`,
+         aiGatewayUrl: `${process.env.REACT_APP_AI_GATEWAY_URL}`,
+      }
+   }
+}
+
+export default awsExports;
