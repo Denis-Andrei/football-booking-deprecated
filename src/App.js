57097b4810c005f9cdaa191fdeceacbaf6184fb1
Denis Peptanariu <denispeptanariu@gmail.com>
2024-05-08 10:45:07 +0100

    added auth ui

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React from 'react';
-import { Amplify } from 'aws-amplify';
 import {Authenticator} from "@aws-amplify/ui-react";
-import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import {Amplify} from "aws-amplify";
+import awsExports from './aws-exports';
 
-
-import { awsConfig } from './aws-config';
-
-Amplify.configure(awsConfig);
-function App() {
-  // const [showResult, setShowResult] = useState(false);
-  // const [apiMessage, setApiMessage] = useState("");
-  //
-  // const getApiMessage = async () => {
-  //
-  //   console.log(process.env.REACT_APP_API_GATEWAY_URL);
-  //   const response = await fetch(`${process.env.REACT_APP_API_GATEWAY_URL}/hello`, {
-  //     mode: 'no-cors'
-  //   });
-  //
-  //   const responseData = await response.text();
-  //   console.log(responseData)
-  //
-  //   setShowResult(true);
-  //   setApiMessage(responseData);
-  // };
-
+Amplify.configure(awsExports);
+function App({Component, pageProps}) {
 
   return (
       <Authenticator>
-        {({ signOut, user }) => (
-            <main>
-              <h1>Hello {user.username}</h1>
-              <button onClick={signOut}>Sign out</button>
-            </main>
-        )}
+          {({ signOut, user }) => (
+              <main>
+                  <h1>Hello {user?.username}</h1>
+                  <button onClick={signOut}>Sign out</button>
+                  <Component {...pageProps} />
+              </main>
+          )}
       </Authenticator>
   );
 }
